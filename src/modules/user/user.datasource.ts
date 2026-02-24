d47df9ab4f1956fe52a81b97da90c49e51460a71
avytran <tr.anhvy.work@gmail.com>
2026-02-24 19:35:11 +0700

    refactor: improve user datasource

--- src/modules/user/user.datasource.ts
+++ src/modules/user/user.datasource.ts
@@ -1,52 +1,29 @@
 import { MANAGEMENT_API_URL } from "../../config/env.js";
 import type { UserTotalBudget, UpdateTotalBudgetInput } from "../../types/user.js";
-
-interface ServiceResponse<T> {
-    status: "success" | "error";
-    data?: T;
-    message?: string;
-}
+import { API } from "../../utils/http.js";
+import { SuccessResult } from "../../types/result.js";
 
 export class UserAPI {
     private baseURL = `${MANAGEMENT_API_URL}/v1`;
+    private api = new API(this.baseURL);
 
-    private buildUserTotalBudgetURL(userId: string): string {
-        return `${this.baseURL}/users/${userId}/total-budget`;
+    private buildUserTotalBudgetPath(userId: string): string {
+        return `/users/${userId}/total-budget`;
     }
 
-    private async request<T>(input: RequestInfo, init?: RequestInit): Promise<ServiceResponse<T>> {
-        const res = await fetch(input, init);
-        const text = await res.text();
+    async getTotalBudget(userId: string, token: string): Promise<SuccessResult<UserTotalBudget>> {
+        const path = this.buildUserTotalBudgetPath(userId);
 
-        if (!res.ok) {
-            throw new Error(`Backend error ${res.status}: ${text}`);
-        }
-
-        return JSON.parse(text);
+        return this.api.get<SuccessResult<UserTotalBudget>>(path, {
+            Authorization: `Bearer ${token}`,
+        })
     }
 
-    async getTotalBudget(userId: string, token: string) {
-        const url = this.buildUserTotalBudgetURL(userId);
+    async updateTotalBudget(userId: string, input: UpdateTotalBudgetInput, token: string): Promise<SuccessResult<UserTotalBudget>> {
+        const path = this.buildUserTotalBudgetPath(userId);
 
-        return this.request<UserTotalBudget>(url, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-        });
-    }
-
-    async updateTotalBudget(userId: string, input: UpdateTotalBudgetInput, token: string) {
-        const url = this.buildUserTotalBudgetURL(userId);
-
-        return this.request<UserTotalBudget>(url, {
-            method: "PATCH",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(input),
-        });
+        return this.api.patch<SuccessResult<UserTotalBudget>>(path, input, {
+            Authorization: `Bearer ${token}`,
+        })
     }
 }
