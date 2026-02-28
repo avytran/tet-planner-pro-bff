74017cde5150993cd2bd337c86b55d05ef0f1a45
avytran <tr.anhvy.work@gmail.com>
2026-02-28 16:33:35 +0700

    feat: add delete all shopping items of user mutation

--- src/modules/shoppingItem/shoppingItem.datasource.ts
+++ src/modules/shoppingItem/shoppingItem.datasource.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { MANAGEMENT_API_URL } from "../../config/env.js";
-import { DeleteShoppingItemOfUserResponse, GetShoppingItemParams, GetShoppingItemsOfUserResponse, ShoppingItem, ShoppingItemInput, UpdateCreateShoppingItemOfUserResponse } from "../../types/shoppingItem.js";
+import { DeleteAllShoppingItemsResponse, DeleteShoppingItemOfUserResponse, GetShoppingItemParams, GetShoppingItemsOfUserResponse, ShoppingItem, ShoppingItemInput, UpdateCreateShoppingItemOfUserResponse } from "../../types/shoppingItem.js";
 import { API } from "../../utils/http.js";
 import { SuccessResult } from "../../types/result.js";
 
@@ -19,7 +19,7 @@ export class ShoppingItemAPI {
 
     async getShoppingItemsOfUser(userId: string, params: GetShoppingItemParams, token: string): Promise<SuccessResult<GetShoppingItemsOfUserResponse>> {
         const query = new URLSearchParams(params as any).toString();
-        
+
         const path = `${this.buildShoppingItemPath(userId)}?${query}`;
 
         return this.api.get<SuccessResult<GetShoppingItemsOfUserResponse>>(path, {
@@ -55,6 +55,14 @@ export class ShoppingItemAPI {
         const path = this.buildShoppingItemPath(userId, itemId);
 
         return this.api.delete<SuccessResult<DeleteShoppingItemOfUserResponse>>(path, {
+            Authorization: `Bearer ${token}`
+        });
+    }
+
+    async deleteAllShoppingItemsOfUser(userId: string, token: string): Promise<SuccessResult<DeleteAllShoppingItemsResponse>> {
+        const path = this.buildShoppingItemPath(userId);
+
+        return this.api.delete<SuccessResult<DeleteAllShoppingItemsResponse>>(path, {
             Authorization: `Bearer ${token}`
         });
     }
